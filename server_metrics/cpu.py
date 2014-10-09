16e1f97985a6ea525562277313aa560a4a05e95d
Martin Brochhaus <mbrochh@gmail.com>
2014-10-09 18:46:59 +0800

    Added ignore_self to get_cpu_usage

--- server_metrics/cpu.py
+++ server_metrics/cpu.py
@@ -1,16 +1,21 @@
 """Utilities for getting CPU related metrics."""
+import os
+
 import decimal
 import commands
 
 
-def get_cpu_usage(user=None):
+def get_cpu_usage(user=None, ignore_self=True):
     """
     Returns the total CPU usage for all available cores.
 
     :param user: If given, returns only the total CPU usage of all processes
       for the given user.
+    :param ignore_self: If ``True`` the process that runs this script will
+      be ignored.
 
     """
+    pid = os.getpid()
     cmd = "ps aux"
     output = commands.getoutput(cmd)
     total = 0
@@ -18,6 +23,8 @@ def get_cpu_usage(user=None):
     largest_process_name = None
     for row in output.split('\n')[1:]:
         row = row.split()
+        if row[1] == str(pid):
+            continue
         if user is None or user == row[0]:
             cpu = decimal.Decimal(row[2])
             if cpu > total:
